980d14239a643e4f7e17ec72c7d6daa2fe0d430b
Hanna0036 <hannaevelyn0301@gmail.com>
2025-01-15 17:20:19 +0700

    upload

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask, render_template, redirect, url_for
+app = Flask (__name__)
+
+@app.route('/')
+def home() : 
+    return render_template('home.html')
+
+@app.route('/information')
+def information():
+    return render_template('information.html')
+
+if __name__ == '__main__':
+    app.run (debug=True)
