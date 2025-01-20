1617cef184d3caaf32aff5327bb6ca64238a798e
Hanna0036 <hannaevelyn0301@gmail.com>
2025-01-20 17:35:56 +0700

    FE/board.js

--- static/js/board.js
+++ static/js/board.js
@@ -1,31 +1,31 @@
-const board = document.querySelector('.board');
 
+const board = document.querySelector('.board');
 const items = [
-    document.getElementById('#item1'),
-    document.getElementById('#item2'),
-    document.getElementById('#item3'),
-    document.getElementById('#item4'),
-    document.getElementById('#item5'),
-    document.getElementById('#item6'),
-    document.getElementById('#item7'),
-    document.getElementById('#item8'),
-    document.getElementById('#item9')
+    document.getElementById('item1'),
+    document.getElementById('item2'),
+    document.getElementById('item3'),
+    document.getElementById('item4'),
+    document.getElementById('item5'),
+    document.getElementById('item6'),
+    document.getElementById('item7'),
+    document.getElementById('item8'),
+    document.getElementById('item9')
 ];
 
+let currentPlayer = 'X'; 
 
 function clickBoard(event) {
-    // Ambil elemen yang diklik
     const clickedItem = event.target;
 
-    // Cek apakah kotak sudah terisi
     if (clickedItem.textContent === '') {
-      clickedItem.textContent = currentPlayer; // Isi dengan X atau O
+        clickedItem.textContent = currentPlayer; 
 
-      // Ubah giliran pemain
-      currentPlayer = (currentPlayer === 'X') ? 'O' : 'X';
+        
+        currentPlayer = (currentPlayer === 'X') ? 'O' : 'X';
     }
 }
 
 items.forEach(item => {
     item.addEventListener('click', clickBoard);
 });
+
